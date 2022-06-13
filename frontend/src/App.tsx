d0e3e44fc4155a2e2ff56d3820c6aa40a1a5a15f
fkymy <yuskefuku@gmail.com>
2022-06-13 21:02:57 +0900

    test api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [endpoint, setEndpoint] = useState('users');
+  const [val, setVal] = useState(endpoint);
+
   return (
     <div className="App">
+      <div>
+        <h1>APIテスト</h1>
+      </div>
+      <div>
+        <h3>http://localhost:3000</h3>
+        <input
+            placeholder="users"
+            value={val}
+            onChange={e => setVal(e.target.value)}
+          />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setEndpoint(val);
+          }}
+        >
+          <span className="">Send</span>
+        </button>
+      </div>
+      <iframe title="test" src={`http://localhost:3000/${endpoint}`} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
