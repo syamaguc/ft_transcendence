fa2160d2f1acb03c4e18ef1f129cd849e9f96501
Yuu Funyu <mfunyu@student.42tokyo.jp>
2022-06-30 15:41:03 +0900

    delete: unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -30,21 +29,6 @@ function App() {
         </button>
       </div>
       <iframe title="test" src={`http://localhost:3000/${endpoint}`} />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
