bcbe28de7c9e8b5d1cc0f9487a856be34bc0666d
syamaguc <programmer2844@gmail.com>
2022-07-13 22:12:13 +0900

    add NODE_ENV environment variables

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -3,12 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
 	const app = await NestFactory.create(AppModule);
 	const configService = new ConfigService();
 
 	const sync = configService.get('DB_SYNC');
+	const e = configService.get('NODE_ENV');
 	console.log(`TypeORM synchronize is [ ${sync} ]`);
+	console.log(`TypeORM synchronize is [ ${e} ]`);
 
 	const port = configService.get('API_PORT');
 	await app.listen(port);
