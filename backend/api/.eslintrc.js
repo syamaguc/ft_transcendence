19081cfca0a1deb780fbdf270f7c277e661aad33
syamaguc <programmer2844@gmail.com>
2022-08-07 16:35:50 +0900

    add rules .eslintrc.js

--- backend/api/.eslintrc.js
+++ backend/api/.eslintrc.js
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/unused-vars': 'off',
   },
 };
