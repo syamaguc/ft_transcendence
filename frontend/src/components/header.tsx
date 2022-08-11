cd41f9383ad8c55f3b42bb555b957ee203cddd42
fkymy <yuskefuku@gmail.com>
2022-08-11 09:10:30 +0900

    modify header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -17,15 +17,15 @@ import {
   MenuItem,
   MenuDivider,
   Link as ChakraLink,
+  useColorMode,
   useColorModeValue,
   useDisclosure,
   chakra,
 } from '@chakra-ui/react'
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+import { SunIcon, MoonIcon, HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 
 import NextLink from 'next/link'
 
-import ThemeToggle from './theme-toggle'
 import { GithubIcon } from '@components/icons'
 
 const Links = ['Users', 'Chat', 'Game']
@@ -37,7 +37,7 @@ const NavLink = ({ children }: { children: ReactNode }) => (
     rounded={'md'}
     _hover={{
       textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      bg: useColorModeValue('gray.100', 'gray.700'),
     }}
     href={'#'}
   >
@@ -46,85 +46,117 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 )
 
 const HeaderContent = () => {
+  const { toggleColorMode: toggleMode } = useColorMode()
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const bgColor = useColorModeValue('white', 'gray.800')
+
+  const text = useColorModeValue('dark', 'light')
+  const SwitchIcon = useColorModeValue(MoonIcon, SunIcon)
 
   return (
     <>
-      <Flex w="100%" h="100%" px="6" align="center" justify="space-between">
-        <IconButton
-          size="md"
-          fontSize="lg"
-          aria-label="Open menu"
-          variant="ghost"
-          color="current"
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-        />
-        <NextLink href="/" passHref>
-          <chakra.a display="block" aria-label="Back to homepage">
-            <Heading size="md" display="block">
+      <Flex w='100%' h='100%' px='6' align='center' justify='space-between'>
+        <NextLink href='/' passHref>
+          <chakra.a display='block' aria-label='Back to homepage'>
+            <Heading size='md' display='block'>
               ft_transcendence
             </Heading>
           </chakra.a>
         </NextLink>
-        <Spacer display={{ base: 'none', md: 'flex' }} />
-        <HStack spacing={4} alignItems="center">
-          <HStack
-            as={'nav'}
-            spacing={4}
-            mr={4}
-            display={{ base: 'none', md: 'flex' }}
-          >
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
-          </HStack>
-          <HStack spacing={4} display={{ base: 'none', md: 'flex' }}>
-            <ChakraLink
-              isExternal
-              aria-label="Go to Github"
-              href={'https://github.com/syamaguc/ft_transcendence/'}
+
+        <Flex
+          justify='flex-end'
+          w='100%'
+          align='center'
+          color='gray.400'
+          maxW='1100px'
+        >
+          <HStack spacing='4'>
+            <HStack
+              as='nav'
+              spacing='4'
+              color='gray.600'
+              _dark={{ color: 'whiteAlpha.600' }}
+              fontWeight='semibold'
+              display={{ base: 'none', md: 'flex' }}
             >
-              <Icon
-                as={GithubIcon}
-                display="block"
-                transition="color 0.2s"
-                w="5"
-                h="5"
-                _hover={{ color: 'gray.600' }}
-              />
-            </ChakraLink>
-            <ThemeToggle />
-          </HStack>
-          <Flex alignItems="center">
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded="full"
-                variant="link"
-                cursor="pointer"
-                minW={0}
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+            <HStack spacing='4' display={{ base: 'none', md: 'flex' }}>
+              <ChakraLink
+                isExternal
+                aria-label='Go to Github'
+                href={'https://github.com/syamaguc/ft_transcendence/'}
               >
-                <Avatar
-                  size="sm"
-                  src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                <Icon
+                  as={GithubIcon}
+                  display='block'
+                  transition='color 0.2s'
+                  w='5'
+                  h='5'
+                  _hover={{ color: 'gray.600' }}
                 />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 1</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 1</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
-        </HStack>
+              </ChakraLink>
+            </HStack>
+          </HStack>
+          <HStack spacing='5'>
+            <IconButton
+              size='md'
+              fontSize='lg'
+              aria-label={`Switch to ${text} mode`}
+              variant='ghost'
+              color='current'
+              ml={{ base: '0', md: '3' }}
+              onClick={toggleMode}
+              icon={<SwitchIcon />}
+            />
+            <IconButton
+              size='md'
+              fontSize='lg'
+              aria-label='Open menu'
+              variant='ghost'
+              color='current'
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            />
+            <Flex alignItems='center' display={{ base: 'none', md: 'flex' }}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded='full'
+                  variant='link'
+                  cursor='pointer'
+                  minW={0}
+                >
+                  <Avatar
+                    size='sm'
+                    src='https://cdn.intra.42.fr/users/default.png'
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Link 1</MenuItem>
+                  <MenuItem>Link 1</MenuItem>
+                  <MenuDivider />
+                  <MenuItem>Link 1</MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+          </HStack>
+        </Flex>
       </Flex>
 
       {isOpen ? (
         <Box pb={4} display={{ md: 'none' }}>
-          <Stack as="nav" spacing={4}>
+          <Stack
+            as='nav'
+            spacing='4'
+            color='gray.600'
+            _dark={{ color: 'whiteAlpha.600' }}
+            fontWeight='semibold'
+          >
             {Links.map((link) => (
               <NavLink key={link}>{link}</NavLink>
             ))}
@@ -140,7 +172,7 @@ const Header = (props: HTMLChakraProps<'header'>) => {
 
   return (
     <chakra.header>
-      <chakra.div height="4.5rem" mx="auto" maxW={maxW} maxWidth={maxWidth}>
+      <chakra.div height='4.5rem' mx='auto' maxW={maxW} maxWidth={maxWidth}>
         <HeaderContent />
       </chakra.div>
     </chakra.header>
