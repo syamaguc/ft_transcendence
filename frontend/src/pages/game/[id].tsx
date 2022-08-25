31d80ea003e93594549be7ecaa630e097280ff89
kotabrog <suzuki.kota314@gmail.com>
2022-08-26 01:21:00 +0900

    decompose useEffect

--- frontend/src/pages/game/[id].tsx
+++ frontend/src/pages/game/[id].tsx
@@ -36,85 +36,85 @@ export default function Game() {
   const keyStatus: KeyStatus = {upPressed: false, downPressed: false};
   let playerStatus: PlayerStatus = {role: 2};
   const router = useRouter();
-  let roomId: string;
-//   let socket;
   const [gameStatus, setGameStatus] = useState<number>(0);
   const [server, setServer] = useState();
 
 
   useEffect(() => {
-    if (didLogRef.current === false && router.isReady) {
-      roomId = router.query.id;
-      console.log(roomId);
+    if (didLogRef.current === false) {
       didLogRef.current = true;
-      const localSocket = io('http://localhost:3000');
-      setServer(localSocket);
-      
-      localSocket.emit("connectServer", roomId);
-      localSocket.on("connectClient", (data) => {
-        playerStatus.role = data;
+      setServer(io('http://localhost:3000'));
+    }
+  }, [])
 
-        const registerButton = () => {
-          if (playerStatus.role == 0) {
-            const startButton = document.getElementById('startButton');
-            const endButton = document.getElementById('endButton');
-            if (startButton) {
-              startButton.addEventListener('click', () => {
-                localSocket.emit('start', {id: roomId});
-              });
-            }
-            if (endButton) {
-              endButton.addEventListener('click', () => {
-                localSocket.emit('retry', {id: roomId});
-              });
-            }
+
+  useEffect(() => {
+    if (!server || !router.isReady) return
+    const roomId = router.query.id;
+    server.emit("connectServer", roomId);
+    server.on("connectClient", (data) => {
+      playerStatus.role = data;
+
+      const registerButton = () => {
+        if (playerStatus.role == 0) {
+          const startButton = document.getElementById('startButton');
+          const endButton = document.getElementById('endButton');
+          if (startButton) {
+            startButton.addEventListener('click', () => {
+              server.emit('start', {id: roomId});
+            });
+          }
+          if (endButton) {
+            endButton.addEventListener('click', () => {
+              server.emit('retry', {id: roomId});
+            });
+          }
+        }
+      };
+
+      server.on('clientMove', (data: GameObject) => {
+        setGameObject(data)
+        setGameStatus(data.gameStatus);
+        if (playerStatus.role <= 1) {
+          if (keyStatus.downPressed || keyStatus.upPressed)
+            server.emit('move', {key: keyStatus, id: roomId});
+        }
+      });
+
+      server.on('gameEnd', (data) => {
+        setGameObject(data.data);
+        setGameStatus(data.data.gameStatus);
+      });
+
+      server.on('gameRetry', (data: GameObject) => {
+        setGameObject(data)
+        setGameStatus(data.gameStatus);
+      });
+
+      if (playerStatus.role <= 1) {
+        const keyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+          console.log(event.code);
+          if (event.code === "ArrowUp") {
+            keyStatus.upPressed = true;
+          } else if (event.code === "ArrowDown") {
+            keyStatus.downPressed = true;
           }
         };
-
-        localSocket.on('clientMove', (data: GameObject) => {
-          setGameObject(data)
-          setGameStatus(data.gameStatus);
-          if (playerStatus.role <= 1) {
-            if (keyStatus.downPressed || keyStatus.upPressed)
-              localSocket.emit('move', {key: keyStatus, id: roomId});
+        const keyUpHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+          console.log(event.code);
+          if (event.code === "ArrowUp") {
+            keyStatus.upPressed = false;
+          } else if (event.code === "ArrowDown") {
+            keyStatus.downPressed = false;
           }
-        });
+        };
+        document.addEventListener("keydown", keyDownHandler);
+        document.addEventListener("keyup", keyUpHandler);
+      }
 
-        localSocket.on('gameEnd', (data) => {
-          setGameObject(data.data);
-          setGameStatus(data.data.gameStatus);
-        });
-
-        localSocket.on('gameRetry', (data: GameObject) => {
-          setGameObject(data)
-          setGameStatus(data.gameStatus);
-        });
-
-        if (playerStatus.role <= 1) {
-          const keyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
-            console.log(event.code);
-            if (event.code === "ArrowUp") {
-              keyStatus.upPressed = true;
-            } else if (event.code === "ArrowDown") {
-              keyStatus.downPressed = true;
-            }
-          };
-          const keyUpHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
-            console.log(event.code);
-            if (event.code === "ArrowUp") {
-              keyStatus.upPressed = false;
-            } else if (event.code === "ArrowDown") {
-              keyStatus.downPressed = false;
-            }
-          };
-          document.addEventListener("keydown", keyDownHandler);
-          document.addEventListener("keyup", keyUpHandler);
-        }
-
-        registerButton();
-      });
-    }
-  }, [router])
+      registerButton();
+    });
+  }, [server, router])
 
 
   return (
