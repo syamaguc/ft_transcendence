ca2290e334f07730e0ba6c77ca93669b543919ab
syamaguc <programmer2844@gmail.com>
2022-08-31 21:07:38 +0900

    add entity in DataSource

--- backend/api/src/app/app.datasource.ts
+++ backend/api/src/app/app.datasource.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm';
 import { User } from '../user/entities/user.entity';
+import { GameHistory } from '../game/entities/gameHistory.entity';
 
 export const AppDataSource = new DataSource({
 	type: 'postgres',
@@ -10,7 +11,7 @@ export const AppDataSource = new DataSource({
 	database: 'ft_transcendence',
 	synchronize: true,
 	logging: true,
-	entities: [User],
+	entities: [User, GameHistory],
 	subscribers: [],
 	migrations: [],
 });
