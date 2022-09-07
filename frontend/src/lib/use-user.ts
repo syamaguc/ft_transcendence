88284ad14e01689ef277becfa8647d11d338615a
fkymy <yuskefuku@gmail.com>
2022-09-07 18:33:37 +0900

    fix useUser

--- frontend/src/lib/use-user.ts
+++ frontend/src/lib/use-user.ts
@@ -6,33 +6,44 @@ import { User } from 'src/types/user'
 
 const API_URL = 'http://localhost:3000'
 
-function fetcher(url: string) {
-  fetch(url, {
-    credentials: 'include',
-  })
-    .then((r) => r.json())
-    .then((data) => {
-      return data
-    })
+async function fetchUser(url: string): Promise<{ user: User | null }> {
+  const res = await fetch(url, { credentials: 'include' })
+
+  const data = await res.json()
+
+  // console.log('fetchUser data:', data)
+
+  if (res.ok) {
+    return { user: data }
+  } else {
+    // Authorization error
+    console.log('fetchUser error:', res.status, res.statusText)
+    return { user: null }
+  }
 }
 
 export function useUser({ redirectTo = '', redirectIfFound = false } = {}) {
-  const { data: user, error } = useSWR<User>(`${API_URL}/api/user/currentUser`)
+  const { data, error } = useSWR(`${API_URL}/api/user/currentUser`, fetchUser)
+  const user = data?.user
+  const finished = Boolean(data)
+  const hasUser = Boolean(user)
 
   useEffect(() => {
+    console.log('useEffect')
     // if no redirect needed, just return (example: already on /dashboard)
     // if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-    if (!redirectTo || !user) return
+    if (!redirectTo || !finished) return
 
     if (
       // If redirectTo is set, redirect if the user was not found.
-      (redirectTo && !redirectIfFound && !user) ||
+      (redirectTo && !redirectIfFound && !hasUser) ||
       // If redirectIfFound is also set, redirect if the user was found
-      (redirectIfFound && user)
+      (redirectIfFound && hasUser)
     ) {
+      console.log('redirecting...')
       Router.push(redirectTo)
     }
-  }, [user, redirectIfFound, redirectTo])
+  }, [redirectTo, redirectIfFound, finished, hasUser])
 
   return error ? null : user
 }
