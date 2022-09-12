2d3071479b8164168ac7adfa8e65d0f5625dc85b
fkymy <yuskefuku@gmail.com>
2022-09-12 21:50:27 +0900

    modify logout menu item

--- frontend/src/components/logout.tsx
+++ frontend/src/components/logout.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, ButtonProps, useToast } from '@chakra-ui/react'
+import { MenuItem, Button, ButtonProps, useToast } from '@chakra-ui/react'
 import { API_URL } from 'src/constants'
 import { useUser } from 'src/lib/use-user'
 
@@ -26,7 +26,7 @@ export function LogoutButton(props: ButtonProps) {
         await mutateUser()
         toast({
           description: 'Successfully logged out.',
-          status: 'success',
+          status: 'info',
           duration: 5000,
           isClosable: true,
         })
@@ -55,5 +55,60 @@ export function LogoutButton(props: ButtonProps) {
     <Button {...props} onClick={handleLogout} isLoading={isLoading}>
       Logout
     </Button>
+  )
+}
+
+export function LogoutMenuItem() {
+  const [isLoading, setIsLoading] = useState(false)
+  const toast = useToast()
+  const { mutateUser } = useUser()
+
+  const handleLogout = async () => {
+    setIsLoading(true)
+
+    try {
+      const res = await fetch(`${API_URL}/api/user/logout`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+
+      if (res.ok) {
+        await mutateUser()
+        toast({
+          description: 'Successfully logged out.',
+          status: 'info',
+          duration: 5000,
+          isClosable: true,
+        })
+      } else {
+        toast({
+          description: 'Internal error occurred',
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      toast({
+        description: 'Failed to logout',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+
+    setIsLoading(false)
+  }
+
+  return (
+    <MenuItem onClick={handleLogout} disabled={isLoading}>
+      Logout
+    </MenuItem>
   )
 }
