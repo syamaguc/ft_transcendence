e3ff09ca063f8cd9e365bb126a6c297a3196dae9
fkymy <yuskefuku@gmail.com>
2022-09-13 18:34:06 +0900

    implement basic info form

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -9,6 +9,7 @@ import {
   FormControl,
   FormLabel,
   FormHelperText,
+  FormErrorMessage,
   Icon,
   Input,
   InputGroup,
@@ -23,6 +24,7 @@ import {
   ModalBody,
   ModalFooter,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react'
 import {
   useState,
@@ -39,8 +41,9 @@ import { User } from 'src/types/user'
 import { useUser } from 'src/lib/use-user'
 import { API_URL } from 'src/constants'
 
-// Avatar Upload
 import { useFormik, FormikErrors } from 'formik'
+
+import PasswordField from '@components/password-field'
 
 interface AvatarFormValues {
   file: File
@@ -179,8 +182,304 @@ const AvatarForm = forwardRef<HTMLElement, AvatarFormProps>((props, ref) => {
   )
 })
 
-function Profile() {
+interface BasicInfoFormValues {
+  username: string
+  email: string
+  password: string
+}
+
+function BasicInfoForm() {
+  const { user, mutateUser } = useUser()
+  const toast = useToast()
+  const modal = useDisclosure()
+  const [modalField, setModalField] = useState('')
+
+  const initialValues: BasicInfoFormValues = {
+    username: user.username,
+    email: user.email,
+    password: '',
+  }
+
+  const validate = (values: BasicInfoFormValues) => {
+    const errors: FormikErrors<BasicInfoFormValues> = {}
+    if (!values.username) {
+      errors.username = 'Required'
+    } else if (values.username.length < 3) {
+      errors.username = 'Must be 3 chracters minimum'
+    } else if (values.username.length > 15) {
+      errors.username = 'Must be 15 characters or less'
+    }
+
+    if (!values.email) {
+      errors.email = 'Required'
+    } else if (
+      !/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address'
+    }
+
+    if (!values.password) {
+      errors.password = 'Required'
+    } else if (values.password.length < 8) {
+      errors.password = 'Must be 8 characters minimum'
+    } else if (values.password.length > 30) {
+      errors.password = 'Must be 30 characters or less'
+    } else if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.test(
+        values.password
+      )
+    ) {
+      errors.password =
+        'Must contain uppercase, lowercase, number and symbol chracter'
+    }
+    return errors
+  }
+
+  const formik = useFormik<BasicInfoFormValues>({
+    initialValues,
+    validate,
+    onSubmit: async (values, actions) => {
+      console.log('submit called')
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.log(JSON.stringify(values, null, 2))
+
+      try {
+        const res = await fetch(`${API_URL}/api/user/settings`, {
+          method: 'PATCH',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: values.username,
+            email: values.email,
+            password: values.password,
+          }),
+        })
+
+        if (res.ok) {
+          toast({
+            description: 'Successfully updated your info.',
+            status: 'success',
+            duration: 5000,
+            isClosable: true,
+          })
+        } else {
+          const data = await res.json()
+          toast({
+            description: data.message
+              ? data.message[0]
+              : 'Internal error occurred',
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+          })
+        }
+      } catch (err) {
+        console.log(err)
+        toast({
+          description: 'Internal error occurred',
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+
+      await mutateUser()
+      actions.setSubmitting(false)
+      modal.onClose()
+    },
+  })
+
+  const modalOnOpen = (field: string) => {
+    setModalField(field)
+    modal.onOpen()
+  }
+
+  const modalOnCloseComplete = () => {
+    formik.setFieldValue('username', initialValues.username)
+    formik.setFieldValue('email', initialValues.email)
+    formik.setFieldValue('password', initialValues.password)
+    setModalField('')
+  }
+
+  return (
+    <form id='form-basic-info' onSubmit={formik.handleSubmit}>
+      <Stack spacing='6'>
+        <FormControl>
+          <FormLabel fontSize='xs' color='gray.400'>
+            USERNAME
+          </FormLabel>
+          <Stack direction='row' spacing='4' align='center'>
+            <Input
+              name='readonly-username'
+              type='text'
+              isReadOnly={true}
+              value={user.username}
+            />
+            <Button size='sm' onClick={() => modalOnOpen('username')}>
+              Edit
+            </Button>
+          </Stack>
+        </FormControl>
+        <FormControl>
+          <FormLabel fontSize='xs' color='gray.400'>
+            EMAIL
+          </FormLabel>
+          <Stack direction='row' spacing='4' align='center'>
+            <Input
+              name='readonly-email'
+              type='email'
+              isReadOnly={true}
+              value={user.email}
+            />
+            <Button size='sm' onClick={() => modalOnOpen('email')}>
+              Edit
+            </Button>
+          </Stack>
+        </FormControl>
+        <Modal
+          isOpen={modal.isOpen}
+          onClose={modal.onClose}
+          onCloseComplete={modalOnCloseComplete}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Change your username</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <Stack spacing='6'>
+                {modalField === 'username' && (
+                  <>
+                    <Text>
+                      Enter a new username and your existing password.
+                    </Text>
+                    <FormControl
+                      isInvalid={
+                        formik.errors.username && formik.touched.username
+                      }
+                    >
+                      <FormLabel
+                        htmlFor='username'
+                        fontSize='xs'
+                        color='gray.400'
+                      >
+                        USERNAME
+                      </FormLabel>
+                      <Input
+                        name='username'
+                        type='text'
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.username}
+                      />
+                      <FormErrorMessage>
+                        {formik.errors.username}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </>
+                )}
+                {modalField === 'email' && (
+                  <>
+                    <Text>Enter a new email and your existing password.</Text>
+                    <FormControl
+                      isInvalid={formik.errors.email && formik.touched.email}
+                    >
+                      <FormLabel htmlFor='email' fontSize='xs' color='gray.400'>
+                        EMAIL
+                      </FormLabel>
+                      <Input
+                        name='email'
+                        type='email'
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.email}
+                      />
+                      <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                    </FormControl>
+                  </>
+                )}
+                <FormControl
+                  isInvalid={formik.errors.password && formik.touched.password}
+                >
+                  <FormLabel htmlFor='password' fontSize='xs' color='gray.400'>
+                    PASSWORD
+                  </FormLabel>
+                  <InputGroup>
+                    <Input
+                      name='password'
+                      type='password'
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                    />
+                  </InputGroup>
+                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                </FormControl>
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                type='submit'
+                form='form-basic-info'
+                colorScheme='blue'
+                isLoading={formik.isSubmitting}
+                mr={3}
+              >
+                Save
+              </Button>
+              <Button onClick={modal.onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Stack>
+    </form>
+  )
+}
+
+type BasicInfoFormProps = {
+  user: User
+}
+
+function BasicInfo() {
   const avatarForm = useDisclosure()
+  const { user, mutateUser } = useUser()
+
+  return (
+    <Stack>
+      <Heading fontSize='2xl'>Basic information</Heading>
+      <Stack
+        borderRadius='xl'
+        direction='row'
+        spacing='8'
+        align='start'
+        py='4'
+        px='8'
+        mx='8'
+      >
+        <Avatar
+          size='xl'
+          src={`${API_URL}/api/user/avatar/${user.profile_picture}`}
+        >
+          <AvatarBadge boxSize='0.9em' bg='green.500' />
+        </Avatar>
+        <Stack direction='column' spacing='2'>
+          <Text fontWeight='600' fontSize='2xl' mt='2'>
+            {user.username}
+          </Text>
+          <Button size='sm' onClick={avatarForm.onOpen}>
+            Upload Avatar
+          </Button>
+          <AvatarForm isOpen={avatarForm.isOpen} onClose={avatarForm.onClose} />
+        </Stack>
+      </Stack>
+      <Divider orientation='horizontal' />
+      <BasicInfoForm />
+    </Stack>
+  )
+}
+
+function Profile() {
   const { user, mutateUser } = useUser()
 
   return (
@@ -192,58 +491,7 @@ function Profile() {
               Profile
             </Heading>
             <Stack spacing='8'>
-              <Stack>
-                <Heading fontSize='2xl'>Basic information</Heading>
-                <Stack
-                  borderRadius='xl'
-                  direction='row'
-                  spacing='8'
-                  align='start'
-                  py='4'
-                  px='8'
-                  mx='8'
-                >
-                  <Avatar
-                    size='xl'
-                    src={`${API_URL}/api/user/avatar/${user.profile_picture}`}
-                  >
-                    <AvatarBadge boxSize='0.9em' bg='green.500' />
-                  </Avatar>
-                  <Stack direction='column' spacing='2'>
-                    <Text fontWeight='600' fontSize='2xl' mt='2'>
-                      {user.username}
-                    </Text>
-                    <Button size='sm' onClick={avatarForm.onOpen}>
-                      Upload Avatar
-                    </Button>
-                  </Stack>
-                </Stack>
-                <Divider orientation='horizontal' />
-                <form>
-                  <Stack spacing='6'>
-                    <FormControl>
-                      <FormLabel fontSize='xs' color='gray.400'>
-                        USERNAME
-                      </FormLabel>
-                      <Input
-                        type='text'
-                        isReadOnly={true}
-                        value={user.username}
-                      />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel fontSize='xs' color='gray.400'>
-                        EMAIL
-                      </FormLabel>
-                      <Input
-                        type='email'
-                        isReadOnly={true}
-                        value={user.email}
-                      />
-                    </FormControl>
-                  </Stack>
-                </form>
-              </Stack>
+              <BasicInfo />
               <Stack>
                 <Heading fontSize='2xl'>Status</Heading>
                 <Text>{user.status}</Text>
@@ -259,7 +507,6 @@ function Profile() {
             </Stack>
             <Text>{JSON.stringify(user)}</Text>
           </Stack>
-          <AvatarForm isOpen={avatarForm.isOpen} onClose={avatarForm.onClose} />
         </Box>
       </Container>
     </Layout>
