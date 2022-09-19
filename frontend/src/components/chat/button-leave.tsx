deef11ff0397aed30895eb5a2bf57a6d3bbabfdb
mfunyu <mfunyu@student.42tokyo.jp>
2022-09-19 21:40:30 +0900

    add: Delete for owner

--- frontend/src/components/chat/button-leave.tsx
+++ frontend/src/components/chat/button-leave.tsx
@@ -1,6 +1,17 @@
-import { Button } from '@chakra-ui/react'
-import { useCallback } from 'react'
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { useCallback, useEffect, useState } from 'react'
 import { Socket } from 'socket.io-client'
+import { useUser } from 'src/lib/use-user'
 import { ChannelObject } from 'src/types/chat'
 
 type Props = {
@@ -9,19 +20,78 @@ type Props = {
   isJoined: boolean
 }
 
+const ButtonDelete = ({ socket, currentRoom }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const onClickDelete = useCallback(() => {
+    if (!currentRoom || !socket) return
+    socket.emit('deleteRoom', currentRoom.id)
+    console.log('deleteRoom', currentRoom.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentRoom])
+
+  return (
+    <>
+      <Button size='sm' m={2} onClick={onOpen}>
+        Delete
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Channel</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Are you sure you want to delete #{currentRoom.name}? This action
+            cannot be undone.
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant='ghost' mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='red' onClick={onClickDelete}>
+              Delete Channel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
 const ButtonLeave = ({ socket, currentRoom, isJoined }: Props) => {
+  const [isOwner, setIsOwers] = useState<boolean>()
+  const { user } = useUser()
+
   const onClickLeave = useCallback(() => {
     if (!currentRoom || !socket) return
     socket.emit('leaveRoom', currentRoom.id)
     console.log('leaveRoom', currentRoom.id)
   }, [currentRoom, socket])
 
+  useEffect(() => {
+    // if (!currentRoom) return
+    console.log(currentRoom.owner)
+    console.log('userId = ', user.userId)
+    if (currentRoom.owner == user.userId) {
+      setIsOwers(true)
+    } else {
+      setIsOwers(false)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentRoom])
+
+  console.log(isOwner)
   return (
     <>
       {isJoined ? (
-        <Button size='sm' m={2} onClick={onClickLeave}>
-          leave
-        </Button>
+        isOwner ? (
+          <ButtonDelete socket={socket} currentRoom={currentRoom} />
+        ) : (
+          <Button size='sm' m={2} onClick={onClickLeave}>
+            Leave
+          </Button>
+        )
       ) : null}
     </>
   )
