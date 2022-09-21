c6c7e90edb8e1fdb6955909d0619f68f4d2456cd
fkymy <yuskefuku@gmail.com>
2022-09-22 02:10:32 +0900

    layout dm friends

--- frontend/src/components/dm/dm-friendslist.tsx
+++ frontend/src/components/dm/dm-friendslist.tsx
@@ -2,25 +2,55 @@ import {
   Avatar,
   AvatarBadge,
   Box,
+  Button,
   Circle,
+  Container,
+  Divider,
   Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Icon,
   Stack,
   Spacer,
   Text,
+  Tab,
+  TabProps,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tooltip,
+  FormControl,
+  FormHelperText,
+  FormErrorMessage,
+  useColorModeValue,
+  useToast,
 } from '@chakra-ui/react'
+
 import NextLink from 'next/link'
-
-import useSWR from 'swr'
-import { fetchUsers } from 'src/lib/fetchers'
-
-import { FiMessageSquare, FiMoreVertical } from 'react-icons/fi'
-
-import { User } from 'src/types/user'
-import { useUser } from 'src/lib/use-user'
-import { API_URL } from 'src/constants'
-import { Socket } from 'socket.io-client'
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import useSWR from 'swr'
+
+import { useFormik, FormikErrors } from 'formik'
+import { FiMessageSquare, FiMoreVertical } from 'react-icons/fi'
+import { BiMessage, BiUserX } from 'react-icons/bi'
+
+import UserStatusBadge from '@components/user-status-badge'
+import { PartialUserInfo, User } from 'src/types/user'
+import { fetchUsers } from 'src/lib/fetchers'
+import { useUser } from 'src/lib/use-user'
+import { useFriends } from 'src/lib/use-friends'
+import { useBlocked } from 'src/lib/use-blocked'
+import { API_URL } from 'src/constants'
+
+import { Socket } from 'socket.io-client'
 
 type Props = {
   socket: Socket
@@ -52,6 +82,139 @@ function DMFriendsList({ socket }: Props) {
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const { friends, countAll, countOnline } = useFriends()
+  const { blocked } = useBlocked()
+
+  const PrimaryTab = (props: TabProps) => {
+    const textColor = useColorModeValue('whiteAlpha.900', 'inherit')
+    const bgColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.400')
+    const selectedColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
+
+    return (
+      <Tab
+        {...props}
+        color={textColor}
+        bg={bgColor}
+        borderRadius='md'
+        px='2'
+        py='1'
+        _selected={{ color: selectedColor, bg: 'inherit' }}
+      />
+    )
+  }
+
+  const SecondaryTab = (props: TabProps) => {
+    const selectedBgColor = useColorModeValue('gray.200', 'gray.500')
+    const hoverBgColor = useColorModeValue('gray.50', 'inherit')
+
+    return (
+      <Tab
+        {...props}
+        borderRadius='md'
+        px='2'
+        py='1'
+        _hover={{ bg: hoverBgColor }}
+        _selected={{ bg: selectedBgColor }}
+      />
+    )
+  }
+
+  return (
+    <Container maxW='2xl' py='12'>
+      <Tabs variant='soft-rounded' colorScheme='gray'>
+        <Stack>
+          <Flex align='center'>
+            <Stack direction='row' align='center' spacing='4'>
+              <Heading as='h1' fontSize='xl'>
+                Friends
+              </Heading>
+              <Box h='24px'>
+                <Divider orientation='vertical' />
+              </Box>
+              <TabList>
+                <SecondaryTab mr='2'>Online</SecondaryTab>
+                <SecondaryTab mr='2'>All</SecondaryTab>
+                <SecondaryTab mr='2'>Blocked</SecondaryTab>
+              </TabList>
+            </Stack>
+            <Spacer />
+            <PrimaryTab>Add Friend</PrimaryTab>
+          </Flex>
+          <TabPanels>
+            <TabPanel>
+              {friends && countOnline > 0 && (
+                <Stack spacing='0'>
+                  <Text
+                    mb='3'
+                    fontSize='sm'
+                    fontWeight='semibold'
+                    color='gray.400'
+                  >
+                    ONLINE - {countOnline}
+                  </Text>
+                  <Divider />
+                  <Stack>
+                    {friends.filter(isOnline).map((friend: PartialUserInfo) => (
+                      <FriendItem key={friend.username} friend={friend} />
+                    ))}
+                  </Stack>
+                </Stack>
+              )}
+            </TabPanel>
+            <TabPanel>
+              {friends && countAll > 0 && (
+                <Stack spacing='0'>
+                  <Text
+                    mb='3'
+                    fontSize='sm'
+                    fontWeight='semibold'
+                    color='gray.400'
+                  >
+                    ALL FRIENDS - {countAll}
+                  </Text>
+                  <Divider />
+                  <Stack>
+                    {friends
+                      .filter(isNotBlocked)
+                      .map((friend: PartialUserInfo) => (
+                        <FriendItem key={friend.username} friend={friend} />
+                      ))}
+                  </Stack>
+                </Stack>
+              )}
+            </TabPanel>
+            <TabPanel>
+              {blocked && blocked.length > 0 && (
+                <Stack spacing='0'>
+                  <Text
+                    mb='3'
+                    fontSize='sm'
+                    fontWeight='semibold'
+                    color='gray.400'
+                  >
+                    BLOCKED - {blocked.length}
+                  </Text>
+                  <Divider />
+                  <Stack>
+                    {blocked.map((blockedUser: PartialUserInfo) => (
+                      <BlockedUserItem
+                        key={blockedUser.username}
+                        user={blockedUser}
+                      />
+                    ))}
+                  </Stack>
+                </Stack>
+              )}
+            </TabPanel>
+            <TabPanel>
+              <AddFriend user={currentUser} />
+            </TabPanel>
+          </TabPanels>
+        </Stack>
+      </Tabs>
+    </Container>
+  )
 
   return (
     <>
